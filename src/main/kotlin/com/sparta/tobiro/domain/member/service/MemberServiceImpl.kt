0178ae37bca2179af48dec95d68ef956063799b7
chanmo2311211 <abczza@naver.com>
2024-01-23 19:33:01 +0900

    ✨Feat: Member 프로필수정기능 추가
    1. Controller updateMemberProfile 작성
    2. Service updateMemberProfile 작성
    3. ServiceImpl updateMemberProfile 작성

--- src/main/kotlin/com/sparta/tobiro/domain/member/service/MemberServiceImpl.kt
+++ src/main/kotlin/com/sparta/tobiro/domain/member/service/MemberServiceImpl.kt
@@ -2,11 +2,14 @@ package com.sparta.tobiro.domain.member.service
 
 import com.sparta.tobiro.domain.member.dto.MemberResponse
 import com.sparta.tobiro.domain.member.dto.MemberSignUpRequest
+import com.sparta.tobiro.domain.member.dto.UpdateMemberProfileRequest
 import com.sparta.tobiro.domain.member.model.Member
 import com.sparta.tobiro.domain.member.model.Role
 import com.sparta.tobiro.domain.member.model.toResponse
 import com.sparta.tobiro.domain.member.repository.MemberRepository
+import com.sparta.tobiro.global.exception.ModelNotFoundException
 import jakarta.transaction.Transactional
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -33,6 +36,16 @@ class MemberServiceImpl(
                 }
             )
         ).toResponse()
+    }
+
+    override fun updateMemberProfile(memberId: Long, request: UpdateMemberProfileRequest): MemberResponse {
+        val member = memberRepository.findByIdOrNull(memberId) ?: throw ModelNotFoundException("Member", memberId)
+        member.name = request.name
+        member.email = request.email
+        member.password = request.password
+        member.introduction = request.introduction
+        member.tlno = request.tlno
+        return memberRepository.save(member).toResponse()
     }
 }
 
