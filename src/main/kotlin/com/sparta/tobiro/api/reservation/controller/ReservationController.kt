2bdc71a88e988008c83bc0f301e30cc19624c5e8
moveuk <v_donguk@naver.com>
2024-01-25 00:59:56 +0900

    ✨Feat: 유저 서비스 - 객실 예약하기 기능
    1. Controller, Service, Repo 로직 및 dto 작성
    2. ServiceImpl 제거하고 Service 클래스로 변경
    3. ReservationResponse from 스태틱 메소드 작성
    4. Reservation.fullCharge Long -> Int 타입 변경

--- src/main/kotlin/com/sparta/tobiro/api/reservation/controller/ReservationController.kt
+++ src/main/kotlin/com/sparta/tobiro/api/reservation/controller/ReservationController.kt
@@ -10,15 +10,17 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/api/v1//reservations")
+@RequestMapping("/api/v1/reservations")
 @RestController
 class ReservationController (
         private val reservationService: ReservationService
 ){
 
     @PostMapping
-    fun createReservation(@RequestBody createReservationRequest: CreateReservationRequest)
-    : ResponseEntity<ReservationResponse?>{
+    fun createReservation(
+        @RequestBody createReservationRequest: CreateReservationRequest
+    )
+    : ResponseEntity<ReservationResponse>{
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(reservationService.createReservation(createReservationRequest))
--- src/main/kotlin/com/sparta/tobiro/domain/reservation/dto/CreateReservationRequest.kt
+++ src/main/kotlin/com/sparta/tobiro/domain/reservation/dto/CreateReservationRequest.kt
@@ -1,4 +1,28 @@
 package com.sparta.tobiro.domain.reservation.dto
 
-class CreateReservationRequest {
+import com.sparta.tobiro.domain.accommodation.model.Room
+import com.sparta.tobiro.domain.member.model.Member
+import com.sparta.tobiro.domain.reservation.model.Reservation
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.Period
+
+data class CreateReservationRequest(
+    val roomId: Long,
+    val checkinDate: LocalDate,
+    val checkoutDate: LocalDate,
+    val occupancy: Int
+) {
+    fun to(member: Member, room: Room): Reservation {
+        val period = Period.between(checkinDate, checkoutDate)
+        val days = period.days
+        return Reservation(
+            checkinDate = checkinDate.atTime(15,0),
+            checkoutDate = checkoutDate.atTime(LocalTime.NOON),
+            occupancy = occupancy,
+            fullCharge = room.price * days,
+            member = member,
+            room = room
+        )
+    }
 }
