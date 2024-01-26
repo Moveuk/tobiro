563e5b07f095e9fc5c15749b939f527d799b27db
qordpsem <qkrrb1223@naver.com>
2024-01-26 15:47:22 +0900

    📝Feat: ReviewResponse에 reviewId 추가

--- src/main/kotlin/com/sparta/tobiro/domain/reservation/dto/ReviewResponse.kt
+++ src/main/kotlin/com/sparta/tobiro/domain/reservation/dto/ReviewResponse.kt
@@ -1,6 +1,7 @@
 package com.sparta.tobiro.domain.reservation.dto
 
 data class ReviewResponse (
+        val reviewId: Int,
         val memberName: String,
         val content: String,
         val rating: Int
